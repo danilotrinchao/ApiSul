2c487feba78e63b645e25c37f38d6d84f14f75fd
Danilo Trinchão <danilo.trinchao@modalgr.com.br>
2021-06-07 13:52:23 -0300

    Remoção de usings desnecessários.

--- TesteAdmissional/Service/ElevadorService.cs
+++ TesteAdmissional/Service/ElevadorService.cs
@@ -1,11 +1,6 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text.Json;
-using System.Threading.Tasks;
-using TesteAdmissional.Contexto;
 using TesteAdmissional.Interface;
 using TesteAdmissional.Models;
 using JsonSerializer = System.Text.Json.JsonSerializer;
